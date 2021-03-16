b71f0e1a8b85d030cc9d85fb08ef744eb430222f
Jason Healy <hello@jasonhealy.io>
2021-03-16 16:49:07 +0000

    fix: allow ethr:did to post messages

--- app/src/components/NewPost.tsx
+++ app/src/components/NewPost.tsx
@@ -78,13 +78,13 @@ const NewPost: React.FC<Props> = (props: Props) => {
     }
   }, [selectedDid, identifiers]);
 
-  useEffect(() => {
-    if (!selectedDid?.startsWith("did:nft:")) {
-      setDisabled(true);
-    } else {
-      setDisabled(false);
-    }
-  }, [selectedDid]);
+  // useEffect(() => {
+  //   if (!selectedDid?.startsWith("did:nft:")) {
+  //     setDisabled(true);
+  //   } else {
+  //     setDisabled(false);
+  //   }
+  // }, [selectedDid]);
 
   const createPost = async () => {
     if (!process.env.REACT_APP_BASE_URL)
@@ -165,8 +165,6 @@ const NewPost: React.FC<Props> = (props: Props) => {
     }, 1000);
   };
 
-  console.log(!!disabled, !!postContent, dmsOpen);
-
   return (
     <div style={{ position: "relative" }}>
       <Card
@@ -199,7 +197,7 @@ const NewPost: React.FC<Props> = (props: Props) => {
                 }}
                 placeholder={
                   !!disabled
-                    ? "Only NFTs can post here!"
+                    ? "Oops, NFTs only allowed around here"
                     : props.recipientDid
                     ? dmsOpen
                       ? "Hey my DMs are open. What's up?"
