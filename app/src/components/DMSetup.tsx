0ce8600ac2ba674cb4a4f1c07f7fe1a607201528
Jason Healy <hello@jasonhealy.io>
2021-03-16 16:09:54 +0000

    style: update messaging endpoint

--- app/src/components/DMSetup.tsx
+++ app/src/components/DMSetup.tsx
@@ -1,99 +1,109 @@
-import React, { useEffect, useState } from 'react'
-import { Typography, Form, Input, Button, List, notification, Card } from 'antd'
-import { useVeramo } from '@veramo-community/veramo-react'
-import { IDIDManager, IResolver, TAgent } from '@veramo/core'
-
+import React, { useEffect, useState } from "react";
+import {
+  Typography,
+  Form,
+  Input,
+  Button,
+  List,
+  notification,
+  Card,
+  Row,
+  Col,
+} from "antd";
+import { useVeramo } from "@veramo-community/veramo-react";
+import { IDIDManager, IResolver, TAgent } from "@veramo/core";
 
 interface FormValues {
-  endpoint: string
+  endpoint: string;
 }
 
 const DMSetup = () => {
-  const [messagingEndpoint, setMessagingEndpoint ] = useState<string | undefined>(undefined)
-  const [owner, setOwner ] = useState<string | undefined>(undefined)
+  const [endpoint, setEndpoint] = useState<string>();
+  const [messagingEndpoint, setMessagingEndpoint] = useState<
+    string | undefined
+  >(undefined);
+  const [owner, setOwner] = useState<string | undefined>(undefined);
 
-  const { getAgent } = useVeramo()
+  const { getAgent } = useVeramo();
 
-  let agent: TAgent<IDIDManager & IResolver> | undefined
+  let agent: TAgent<IDIDManager & IResolver> | undefined;
 
   try {
     agent = getAgent("web3Agent");
   } catch (e) {}
 
   useEffect(() => {
-
     if (agent) {
-      agent.didManagerGetByAlias({alias: 'owner'})
-      .then(owner => {
-        setOwner(owner.did)
-        return owner
-      })
-      .then(({did}) => agent?.resolveDid({ didUrl: did }))
-      .then((didDoc) => {
-        const service = didDoc?.service?.find((s) => s.type === 'Messaging')
-        if (service) {
-          setMessagingEndpoint(service.serviceEndpoint)
-        }
-      })
+      agent
+        .didManagerGetByAlias({ alias: "owner" })
+        .then((owner) => {
+          setOwner(owner.did);
+          return owner;
+        })
+        .then(({ did }) => agent?.resolveDid({ didUrl: did }))
+        .then((didDoc) => {
+          const service = didDoc?.service?.find((s) => s.type === "Messaging");
+          if (service) {
+            setMessagingEndpoint(service.serviceEndpoint);
+          }
+        });
     }
-  }, [agent])
+  }, [agent]);
 
-  const initialValues: FormValues = {
-    endpoint: '',
-  }
-
-  const addService = async (values: FormValues) => {
-
+  const addService = async () => {
     try {
-      if (owner && values.endpoint) {
+      if (owner && endpoint) {
         await agent?.didManagerAddService({
           did: owner,
           service: {
-            id: 'msg123',
-            type: 'Messaging',
-            serviceEndpoint: values.endpoint,
-            description: '',
+            id: "msg123",
+            type: "Messaging",
+            serviceEndpoint: endpoint,
+            description: "",
           },
-        })
-        notification.success({message: 'Direct messages enabled'})
+        });
+        notification.success({ message: "Direct messages enabled" });
       }
     } catch (error) {
-      notification.error({message: 'Failed'})
+      notification.error({ message: "Failed" });
     }
-  }
+  };
 
   if (messagingEndpoint) {
-    return (
-      <Card>Your messages are handled by: {messagingEndpoint}</Card>
-    )
+    return <Card>Your messages are handled by: {messagingEndpoint}</Card>;
   }
 
   return (
-    <Card title='Enable direct messages'>
-
-      <Form
-        initialValues={initialValues}
-        onFinish={addService}
-        onFinishFailed={(e) => notification.error({message: 'Failed'})}
-      >
-        <Form.Item
-          label="Endpoint"
-          name="endpoint"
-          rules={[{ required: true, message: 'Please input Endpoint!' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item >
-          <Button type="primary" htmlType="submit">
+    <Card title="Enable direct messages" bordered={false}>
+      <Typography.Text>
+        Update your DID dcouments service endpoint to ebanle direct messaging.
+      </Typography.Text>
+      <Row style={{ alignItems: "center", marginTop: 15 }}>
+        <Col flex={1} style={{ marginRight: 20 }}>
+          <Input
+            value={endpoint}
+            onChange={(e) => setEndpoint(e.target.value)}
+            placeholder="Messaging URL"
+            style={{
+              padding: 10,
+              fontSize: 16,
+              borderRadius: 10,
+            }}
+          />
+        </Col>
+        <Col>
+          <Button
+            type="primary"
+            size="large"
+            disabled={!endpoint}
+            onClick={addService}
+          >
             Submit
           </Button>
-          
-        </Form.Item>
-      </Form>
-
+        </Col>
+      </Row>
     </Card>
-  )
-}
+  );
+};
 
-export default DMSetup
+export default DMSetup;
