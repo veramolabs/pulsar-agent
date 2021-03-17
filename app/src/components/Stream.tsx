5b590524e5c40ea6ed891ef86f6f80b0f02b3248
Jason Healy <hello@jasonhealy.io>
2021-03-17 11:34:49 +0000

    feat: show posts on profile

--- app/src/components/Stream.tsx
+++ app/src/components/Stream.tsx
@@ -16,6 +16,7 @@ interface Props {
   refetch?: boolean;
   agent: TAgent<IDataStoreORM>;
   privateMode?: boolean;
+  did?: string;
 }
 
 const Stream: React.FC<Props> = ({
@@ -23,18 +24,33 @@ const Stream: React.FC<Props> = ({
   refetch,
   agent,
   privateMode,
+  did,
 }) => {
   const history = useHistory();
-  const { data: credentials, refetch: refetchQuery } = useQuery(
+
+  const typeQuery = [
+    {
+      column: "type",
+      value: ["VerifiableCredential,VerifiableSocialPosting"],
+    },
+  ];
+
+  const didQuery = [
+    {
+      column: "issuer",
+      value: [did],
+    },
+  ];
+
+  // @ts-ignore
+  const query = did ? typeQuery.concat(didQuery) : typeQuery;
+
+  const { data: credentials, isLoading, refetch: refetchQuery } = useQuery(
     ["credentials", { agentId: agent?.context.name }],
     () =>
       agent.dataStoreORMGetVerifiableCredentials({
-        where: [
-          {
-            column: "type",
-            value: ["VerifiableCredential,VerifiableSocialPosting"],
-          },
-        ],
+        // @ts-ignore
+        where: query,
         order: [{ column: "issuanceDate", direction: "DESC" }],
       })
   );
@@ -50,104 +66,113 @@ const Stream: React.FC<Props> = ({
     <List
       itemLayout="vertical"
       size="large"
-      dataSource={credentials}
-      renderItem={(item) => (
-        <List.Item
-          className="main-post"
-          actions={
-            !privateMode
-              ? [
+      dataSource={credentials?.filter((item) => {
+        return did ? item.verifiableCredential.issuer.id == did : item;
+      })}
+      loading={isLoading}
+      renderItem={(item) => {
+        return (
+          <List.Item
+            className="main-post"
+            actions={
+              !privateMode
+                ? [
+                    <Button
+                      shape="circle"
+                      style={{ border: 0 }}
+                      icon={<CommentOutlined />}
+                      onClick={async () => {
+                        try {
+                          navigator.clipboard.writeText(
+                            `${item.verifiableCredential.id}`
+                          );
+                          notification.success({ message: "URL copied!" });
+                        } catch (err) {
+                          notification.error({ message: err.message });
+                        }
+                      }}
+                    />,
+                    <Button
+                      shape="circle"
+                      style={{ border: 0 }}
+                      icon={<HeartOutlined />}
+                      onClick={async () => {
+                        try {
+                          navigator.clipboard.writeText(
+                            `${item.verifiableCredential.id}`
+                          );
+                          notification.success({ message: "URL copied!" });
+                        } catch (err) {
+                          notification.error({ message: err.message });
+                        }
+                      }}
+                    />,
+                    <Button
+                      shape="circle"
+                      style={{ border: 0 }}
+                      icon={<ShareAltOutlined />}
+                      onClick={async () => {
+                        try {
+                          navigator.clipboard.writeText(
+                            `${item.verifiableCredential.id}`
+                          );
+                          notification.success({ message: "URL copied!" });
+                        } catch (err) {
+                          notification.error({ message: err.message });
+                        }
+                      }}
+                    />,
+                  ]
+                : []
+            }
+          >
+            <Card bordered={false} style={{ width: "100%" }}>
+              <List.Item.Meta
+                avatar={
+                  <Avatar
+                    src={
+                      item.verifiableCredential.credentialSubject.author?.image
+                    }
+                    size="large"
+                  />
+                }
+                title={
                   <Button
-                    shape="circle"
-                    style={{ border: 0 }}
-                    icon={<CommentOutlined />}
-                    onClick={async () => {
-                      try {
-                        navigator.clipboard.writeText(
-                          `${item.verifiableCredential.id}`
-                        );
-                        notification.success({ message: "URL copied!" });
-                      } catch (err) {
-                        notification.error({ message: err.message });
-                      }
-                    }}
-                  />,
-                  <Button
-                    shape="circle"
-                    style={{ border: 0 }}
-                    icon={<HeartOutlined />}
-                    onClick={async () => {
-                      try {
-                        navigator.clipboard.writeText(
-                          `${item.verifiableCredential.id}`
-                        );
-                        notification.success({ message: "URL copied!" });
-                      } catch (err) {
-                        notification.error({ message: err.message });
-                      }
-                    }}
-                  />,
-                  <Button
-                    shape="circle"
-                    style={{ border: 0 }}
-                    icon={<ShareAltOutlined />}
-                    onClick={async () => {
-                      try {
-                        navigator.clipboard.writeText(
-                          `${item.verifiableCredential.id}`
-                        );
-                        notification.success({ message: "URL copied!" });
-                      } catch (err) {
-                        notification.error({ message: err.message });
-                      }
-                    }}
-                  />,
-                ]
-              : []
-          }
-        >
-          <Card bordered={false} style={{ width: "100%" }}>
-            <List.Item.Meta
-              avatar={
-                <Avatar
-                  src={
-                    item.verifiableCredential.credentialSubject.author?.image
-                  }
-                  size="large"
-                />
-              }
-              title={
-                <Button
-                  type="text"
-                  style={{padding: 0, fontSize: 18}}
-                  onClick={() =>
-                    history.push(
-                      "/profile/" +
-                        item.verifiableCredential.credentialSubject.author?.id
-                    )
-                  }
-                >
-                  {item.verifiableCredential.credentialSubject.author?.name}
-                </Button>
-              }
-              description={`${formatDistanceToNow(
-                Date.parse(item.verifiableCredential.issuanceDate)
-              )} ago`}
-            />
-            <div
-              className={"clickable-content"}
-              style={{ paddingTop: 15, fontSize: "1.3rem", marginLeft: 72 }}
-              onClick={() =>
-                history.push(
-                  "/post/" + item?.verifiableCredential.id?.split("/").pop()
-                )
-              }
-            >
-              {item.verifiableCredential.credentialSubject?.articleBody}
-            </div>
-          </Card>
-        </List.Item>
-      )}
+                    type="text"
+                    style={{ padding: 0, fontSize: 18 }}
+                    onClick={() =>
+                      history.push(
+                        "/profile/" +
+                          item.verifiableCredential.credentialSubject.author?.id
+                      )
+                    }
+                  >
+                    {item.verifiableCredential.credentialSubject.author?.name}
+                  </Button>
+                }
+                description={`${formatDistanceToNow(
+                  Date.parse(item.verifiableCredential.issuanceDate)
+                )} ago`}
+              />
+              <div
+                className={"clickable-content"}
+                style={{
+                  paddingTop: 15,
+                  fontSize: "1.3rem",
+                  marginLeft: 72,
+                }}
+                onClick={() =>
+                  history.push(
+                    "/post/" + item?.verifiableCredential.id?.split("/").pop()
+                  )
+                }
+              >
+                {item.verifiableCredential.credentialSubject?.articleBody}
+              </div>
+            </Card>
+          </List.Item>
+        );
+      }}
     />
   );
 };
